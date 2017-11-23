f68780f539e9b7cfbcceb15e10d073f4d08a9c16
huang <huangdey973@gmail.com>
2017-11-23 14:41:58 +0100

    commit mister-cocktail

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,34 @@
+class DosesController < ApplicationController
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to @dose.cocktail
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+  end
+
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description_id, :ingredient_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
+  # get 'doses/new'
+
+  # get 'doses/create'
+
+  # get 'doses/destroy'
+
+  # get 'cocktails/index'
+
+  # get 'cocktails/show'
+
+  # get 'cocktails/new'
+
+  # get 'cocktails/create'
+
+  resources :cocktails do
+    resources :doses, only: [ :index, :new, :create, :show ]
+  end
+  resources :doses, only: [ :new, :create, :destroy ]
+
+  # resources :cocktails
+  # resources :doses
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
